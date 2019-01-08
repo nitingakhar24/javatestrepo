6a9a03cb92aea04528c62d352cdc9371ffb2e0f3
Nitin Gakhar <nitin_gakhar@epam.com>
2019-01-08 02:13:29 +0100

    java program to find common elements between two arrays

---
+++ src/com/sample/common/CommonElements.java
@@ -0,0 +1,29 @@
+package com.sample.common;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CommonElements {
+
+	public static void main(String[] args) {
+	
+	   String[] inputArray1 = {"ONE", "TWO", "THREE", "FOUR", "FIVE", "FOUR"};
+	    String[] inputArray2 = {"THREE", "FOUR", "FIVE", "SIX", "SEVEN", "FOUR"};
+	    
+	    Set<String> set = new HashSet<String>();
+
+	    for(int i = 0; i < inputArray1.length; i++)
+	    {
+	    	for(int j = 0; j < inputArray2.length; j++)
+	    	{
+	    		if(inputArray1[i] == inputArray2[j])
+	    		{
+	    			set.add(inputArray1[i]);
+	    		}
+	    	}
+	    }
+	    
+	    System.out.println(set); 
+	    
+}
+}
