94691260a6ab22f994e45e0b9b19076fd7c27631
Nitin Gakhar <nitin_gakhar@epam.com>
2019-01-09 08:13:43 +0100

    adding descriptions for bubble and selection sort

--- src/com/sample/sort/BubbleSort.java
+++ src/com/sample/sort/BubbleSort.java
@@ -1,23 +1,48 @@
 package com.sample.sort;
 
+
+/*Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order.
+Example:
+First Pass:
+( 5 1 4 2 8 ) –> ( 1 5 4 2 8 ), Here, algorithm compares the first two elements, and swaps since 5 > 1.
+( 1 5 4 2 8 ) –>  ( 1 4 5 2 8 ), Swap since 5 > 4
+( 1 4 5 2 8 ) –>  ( 1 4 2 5 8 ), Swap since 5 > 2
+( 1 4 2 5 8 ) –> ( 1 4 2 5 8 ), Now, since these elements are already in order (8 > 5), algorithm does not swap them.
+
+Second Pass:
+( 1 4 2 5 8 ) –> ( 1 4 2 5 8 )
+( 1 4 2 5 8 ) –> ( 1 2 4 5 8 ), Swap since 4 > 2
+( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
+( 1 2 4 5 8 ) –>  ( 1 2 4 5 8 )
+Now, the array is already sorted, but our algorithm does not know if it is completed. The algorithm needs one whole pass without any swap to know it is sorted.
+
+Third Pass:
+( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
+( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
+( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
+( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
+
+
+Although the algorithm is simple, it is too slow and impractical for most problems even when compared to insertion sort.
+Bubble sort can be practical if the input is in mostly sorted order with some out-of-order elements nearly in position.
+*/
 public class BubbleSort 
 {
 
 	public static void main(String[] args) 
 	{
 		int arr[] = { 36, 19, 26, 12, 5 };
-		int temp = 0;
 
-		performSort(arr, temp);
+		performSort(arr);
 		printValues(arr);
 
 	}
 
-	private static void performSort(int arr[], int temp) 
-	{
+	private static void performSort(int arr[]) {
+		int temp = 0;
 		for (int i = 0; i < arr.length; i++) 
 		{
-			boolean swapflag = false;
+			boolean swapFlag = false;
 			for (int j = 0; j < arr.length - 1 - i; j++) 
 			{
 				if (arr[j] > arr[j + 1]) 
@@ -25,10 +50,10 @@ public class BubbleSort
 					temp = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = temp;
-					swapflag = true;
+					swapFlag = true;
 				}
 			}
-			if (!swapflag) 
+			if (!swapFlag)
 			{
 				break;
 			}
